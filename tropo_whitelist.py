a6caf51ae38472836fe90269210300b200e6f003
Morgan Martin <morganm5201@gmail.com>
2016-11-16 16:32:55 -0500

    create python tropo_whitelist

---
+++ tropo_whitelist.py
@@ -0,0 +1,25 @@
+from subprocess import check_output
+
+def print_whitelist():
+    '''Retrieves tropo's whitelist and prints to STDOUT.
+
+    Returns: string
+    '''
+    response = get_whitelist_response()
+    lines = response.decode().split("\n")
+    for line in lines:
+        if "\"" in line:
+            cidr_block = line.split("\"")[1]
+            print("tropo," + cidr_block)
+
+def get_whitelist_response():
+    '''Retrieves Tropo's whitelist
+
+    Returns: string
+    '''
+    return check_output(["nslookup", "-q=TXT",
+                                    "_netblocks.tropo.com",
+                                    "8.8.8.8"])
+
+if __name__ == '__main__':
+    print_whitelist()

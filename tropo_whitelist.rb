c639eafab577fbb41c459fd2c52c0921cde0841d
Morgan Martin <morganm5201@gmail.com>
2016-11-16 16:32:38 -0500

    create ruby tropo_whitelist

---
+++ tropo_whitelist.rb
@@ -0,0 +1,19 @@
+# Retrieves Tropo's current IP info for whitelisting
+class TropoWhitelist
+  def self.print_whitelist
+    response = self.get_whitelist_response
+    response.split("\n").each do |line|
+      cidr_block = line.split('"')[1]
+      puts "tropo," + cidr_block if cidr_block
+    end
+  end
+
+  private
+
+  # Requires that nslookup tool be installed on host computer
+  def self.get_whitelist_response
+    `nslookup -q=TXT _netblocks.tropo.com 8.8.8.8`
+  end
+end
+
+TropoWhitelist.print_whitelist
